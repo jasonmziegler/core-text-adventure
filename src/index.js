e946cddedd3065f9c84c76651d4c176e3211fdb5
jasonmziegler <jason@insightfulautomation.com>
2019-06-20 22:48:06 -0500

    add webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-function component() {
-    const element = document.createElement('div');
+// function component() {
+//     const element = document.createElement('div');
 
-    element.innerHTML = "Hello World";
+//     element.innerHTML = "Hello World";
+
+//     return element;
+// }
+
+
+
+function createElement (tag = 'div', {
+    content = 'Very default',
+    classNames = ['module-text', 'special']
+} = {}) {
+    const element = document.createElement(tag);
+    const text = document.createTextNode(content);
+
+    element.classList.add(...classNames);
+    element.appendChild(text);
 
     return element;
 }
-
-document.body.appendChild(component());
+const greeting = createElement('p', { content = 'Hello World!', classNames = ['very-big', 'primary-color']});
+document.body.appendChild(greeting);
